a9ef623ba153ec68bf3a966a8858cc8a1126a27c
Michal Czaplinski <mmczaplinski@gmail.com>
2023-04-03 17:23:59 +0200

    The packaged extension

--- blocks/likes-number/store.js
+++ blocks/likes-number/store.js
@@ -1,4 +1,16 @@
 export default {
+  state: {
+    likesCount: 0,
+  },
+  actions: {
+    toggleMovie: ({ state, context }) => {
+      const index = state.wpmovies.likedMovies.findIndex(
+        (post) => post === context.post.id
+      );
+      if (index === -1) state.wpmovies.likedMovies.push(context.post.id);
+      else state.wpmovies.likedMovies.splice(index, 1);
+    },
+  },
   selectors: {
     wpmovies: {
       likesCount: ({ state }) => state.wpmovies.likedMovies.length,
