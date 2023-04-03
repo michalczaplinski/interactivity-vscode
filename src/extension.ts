b2cec2e63c1d8a1c4923055d03b766057fafdf24
Michal Czaplinski <mmczaplinski@gmail.com>
2023-04-03 15:45:20 +0200

    Get the path to the file

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,9 @@ export async function activate(context: vscode.ExtensionContext) {
   const provider = vscode.languages.registerCompletionItemProvider(
     "php",
     {
-      provideCompletionItems(
+      async provideCompletionItems(
         document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken,
-        context: vscode.CompletionContext
+        position: vscode.Position
       ) {
         // get all text until the `position` and check if it reads `console.`
         // and if so then complete if `log`, `warn`, and `error`
@@ -20,6 +18,11 @@ export async function activate(context: vscode.ExtensionContext) {
         }
 
         const uri = document.uri;
+        // Get the path to the directory containing the document
+        const dir = uri.path.substring(0, uri.path.lastIndexOf("/"));
+        const storeFilePath = dir + "/store.js";
+
+        // Here we should use TSC to get the file and parse it
 
         return [
           new vscode.CompletionItem(
